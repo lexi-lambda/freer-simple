e0886b0f7964550262ae003c3c4c173e491fbfa3
Alej Cabrera <cpp.cabrera@gmail.com>
2015-09-14 00:36:58 -0500

    Open.Union: use GHC.TypeLits.Nat

--- src/Data/Open/Union.hs
+++ src/Data/Open/Union.hs
@@ -7,6 +7,30 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FlexibleContexts #-}
 {-# LANGUAGE UndecidableInstances #-}
 
+{-|
+Module      : Data.Open.Union
+Description :
+Copyright   : Alej Cabrera 2015
+License     : BSD-3
+Maintainer  : cpp.cabrera@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+Open unions (type-indexed co-products) for extensible effects. This
+implementation relies on _closed_ type families added to GHC 7.8. It
+has NO overlapping instances and NO Typeable. Alas, the absence of
+Typeable means the projections and injections generally take linear
+time.  The code illustrate how to use closed type families to
+disambiguate otherwise overlapping instances.
+
+The interface is the same as of other OpenUnion*.hs
+
+The data constructors of Union are not exported
+
+Essentially, the nested Either data type
+
+-}
+
 -- repackaging of: http://okmij.org/ftp/Haskell/extensible/OpenUnion41.hs
 module Data.Open.Union (
   Union(..),
@@ -15,35 +39,11 @@ module Data.Open.Union (
   Member(..)
 ) where
 
+import GHC.TypeLits
 
--- |
--- Open unions (type-indexed co-products) for extensible effects
--- This implementation relies on _closed_ type families added
--- to GHC 7.8
--- It has NO overlapping instances and NO Typeable
--- Alas, the absence of Typeable means the projections and injections
--- generally take linear time.
--- The code illustrate how to use closed type families to
--- disambiguate otherwise overlapping instances.
-
--- The interface is the same as of other OpenUnion*.hs
-
--- The data constructors of Union are not exported
-
--- Essentially, the nested Either data type
--- t can be a GADT and hence not necessarily a Functor
--- r: the list of Effect requests
--- v: value type ???
 data Union (r :: [ * -> * ]) v where
   UNow  :: t v -> Union (t ': r) v
   UNext :: Union r v -> Union (any ': r) v
-
-{-
-instance Functor (Union r) where
-    {-# INLINE fmap #-}
-    fmap f (UNow x)  = UNow (f x)
-    fmap f (UNext x) = UNext (fmap f x)
--}
 
 data P (n :: Nat) = P
 
@@ -52,12 +52,12 @@ class Member' t r (n :: Nat) where
   inj' :: P n -> t v -> Union r v
   prj' :: P n -> Union r v -> Maybe (t v)
 
-instance (r ~ (t ': r')) => Member' t r 'Z where
+instance (r ~ (t ': r')) => Member' t r 0 where
   inj' _ = UNow
   prj' _ (UNow x) = Just x
   prj' _ _        = Nothing
 
-instance (r ~ (t' ': r'), Member' t r' n) => Member' t r ('S n) where
+instance (r ~ (t' ': r'), Member' t r' n) => Member' t r n where
   inj' _ = UNext . inj' (P::P n)
   prj' _ (UNow _)  = Nothing
   prj' _ (UNext x) = prj' (P::P n) x
@@ -79,15 +79,13 @@ decomp (UNext v) = Left v
 weaken :: Union r w -> Union (any ': r) w
 weaken = UNext
 
-data Nat = Z | S Nat
-
 -- Find an index of an element in a `list'
 -- The element must exist
 -- This closed type family disambiguates otherwise overlapping
 -- instances
 type family FindElem (t :: * -> *) r :: Nat where
-  FindElem t (t ': r)  = 'Z
-  FindElem t (any ': r)  = 'S (FindElem t r)
+  FindElem t (t ': r)  = 0
+  FindElem t (any ': r)  = FindElem t r
 
 type family EQU (a :: k) (b :: k) :: Bool where
   EQU a a = 'True
