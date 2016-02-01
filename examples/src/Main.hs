72c19abc4f98ad532e44ddee7e187ce2e57c5d7e
Allele Dev <allele.dev@gmail.com>
2016-02-01 21:47:14 +0000

    flesh out example

--- examples/src/Main.hs
+++ examples/src/Main.hs
@@ -1,4 +1,18 @@
+{-# LANGUAGE DataKinds #-}
 module Main where
 
+import Control.Monad.Freer
+import Teletype
+
+runner :: Eff '[Teletype] ()
+runner = do
+  x <- getLine'
+  putStrLn' x
+  y <- getLine'
+  putStrLn' y
+
 main :: IO ()
-main = print "placeholder"
+main = do
+  let xs = runTeletypePure ["cat", "fish"] runner
+  print xs
+  runTeletype runner
